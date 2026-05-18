e9e2ab14bb24c8f19131c382225f01e635b03ada
Zainab Ometere <zainabometere621@gmail.com>
2026-05-19 00:01:14 +0100

    feat: I created the context file and created function for saveLooks and removelooks

--- lib/Context.tsx
+++ lib/Context.tsx
@@ -1,19 +1,38 @@
-"use client"
+"use client";
 
-import { createContext, useContext, useState} from "react"
-import { Look } from "./types"
-//  what the context contains
-interface SavedContextType{
-    savedLooks: Look[]
-    saveLook: (look: Look) => void
-    removeLook: (id: number) => void
+import { createContext, useContext, useState } from "react";
+import { Look } from "./types";
+
+interface SavedContextType {
+  savedLooks: Look[];
+  saveLook: (look: Look) => void;
+  removeLook: (id: number) => void;
 }
 
-//  context
+const SavedContext = createContext<SavedContextType | null>(null);
 
-const SavedContext = createContext<SavedContextType | null> (null)
+export function SavedProvider({ children }: { children: React.ReactNode }) {
+  const [savedLooks, setSavedLooks] = useState<Look[]>([]);
 
-//  provider
-export function SavedProvider ({ children }: {children: React.ReactNode}) {
-    
+  // saveLook functioon
+  const saveLook = (look: Look) => {
+    setSavedLooks([...savedLooks, look]);
+  };
+
+  // your removeLook function goes here
+const removeLook =(id: number) =>{
+    setSavedLooks(savedLooks.filter((look) => look.id !== id ))
+}
+
+  return (
+    <SavedContext.Provider value={{ savedLooks, saveLook, removeLook }}>
+      {children}
+    </SavedContext.Provider>
+  );
+}
+
+export function useSaved() {
+  const context = useContext(SavedContext);
+  if (!context) throw new Error("useSaved must be used within SavedProvider");
+  return context;
 }
